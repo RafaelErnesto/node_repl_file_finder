fea99cea965a58f10e064532b34b548624146b72
Rafael <rflernesto@gmail.com>
2022-01-17 22:57:38 -0400

    feat: add basic features fo file system

--- src/file_system.js
+++ src/file_system.js
@@ -1,5 +1,26 @@
-const fs = require('fs/promises');
+const fs = require('fs/promises')
+const path = require('path')
 
-function findFile(fileName) {
+const getFolderContents = async (currentFolder) => {
+    const folders = await fs.readdir(currentFolder, { withFileTypes: true })
+    console.log(folders);
+}
+
+const filterDirectories = (result) => {
+    return result.filter(item => item.isDirectory())
+}
+
+const filterFiles = (result) => {
+    return result.filter(item => !item.isDirectory())
+}
+
+const lookForFile = (fileName, files) => {
+    const searchResult = files.filter((file) => file === filename)
+    return searhForFile.length > 0 ? searhForFile[0] : undefined
+}
+
+const search = (fileName) => {
 
 }
+
+module.exports.search = search
